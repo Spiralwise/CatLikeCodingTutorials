c5b9f24b32df6416824abe79f60369dd245bcbb0
Spiralwise <contact@spiralwise.net>
2017-06-04 00:39:30 +0200

    Camera Controller includes a look at target

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -5,10 +5,21 @@ using System.Collections;
 public class CameraController : MonoBehaviour {
 
 	public int sensivity = 100;
+	public GameObject cameraTarget;
+
+	Vector3 target;
+
+	void Start () {
+		if (cameraTarget != null)
+			target = cameraTarget.transform.position;
+		else
+			target = Vector3.zero;
+		transform.LookAt (target);
+	}
 
 	void Update () {
 		if (Input.GetMouseButton(0))
-			transform.RotateAround (Vector3.zero, Vector3.up, Input.GetAxis("Mouse X") * sensivity * Time.deltaTime);
+			transform.RotateAround (target, Vector3.up, Input.GetAxis("Mouse X") * sensivity * Time.deltaTime);
 		transform.Translate (Vector3.forward * Input.GetAxis ("Mouse ScrollWheel") * sensivity * Time.deltaTime);
 	}
 }
