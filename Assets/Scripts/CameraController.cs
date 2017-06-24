47cae996fe32227ce150c3c578864ffd3df21b2e
Spiralwise <contact@spiralwise.net>
2017-06-24 15:11:57 +0200

    Update camera controler
    Now can translate with LBM and rotate with ALT+LMB

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -4,10 +4,11 @@ using System.Collections;
 [RequireComponent(typeof(Camera))]
 public class CameraController : MonoBehaviour {
 
-	public int sensivity = 100;
+	public int sensivity = 1000;
+	public int scrollSensivity = 5000;
 	public GameObject cameraTarget;
 
-	Vector3 target;
+	public Vector3 target;
 
 	void Start () {
 		if (cameraTarget != null)
@@ -15,11 +16,27 @@ public class CameraController : MonoBehaviour {
 		else
 			target = Vector3.zero;
 		transform.LookAt (target);
+		if (cameraTarget != null)
+			cameraTarget.transform.SetParent (transform);
 	}
 
 	void Update () {
-		if (Input.GetMouseButton(0))
-			transform.RotateAround (target, Vector3.up, Input.GetAxis("Mouse X") * sensivity * Time.deltaTime);
-		transform.Translate (Vector3.forward * Input.GetAxis ("Mouse ScrollWheel") * sensivity * Time.deltaTime);
+		if (cameraTarget != null)
+			target = cameraTarget.transform.position;
+		float mouseX = Input.GetAxis ("Mouse X");
+		float mouseY = Input.GetAxis ("Mouse Y");
+		if (Input.GetMouseButton (0)) {
+			if (Input.GetKey (KeyCode.LeftAlt))
+				transform.RotateAround (target, Vector3.up, mouseX * sensivity * Time.deltaTime);
+			else {
+				Vector3 forward = Vector3.ProjectOnPlane (transform.forward, Vector3.up);
+				transform.Translate (new Vector3(
+					-mouseX * sensivity * Time.deltaTime,
+					0f,
+					0f));
+				transform.position -= mouseY * forward.normalized * sensivity * Time.deltaTime;
+			}
+		}
+		transform.Translate (Vector3.forward * Input.GetAxis ("Mouse ScrollWheel") * scrollSensivity * Time.deltaTime);
 	}
 }
